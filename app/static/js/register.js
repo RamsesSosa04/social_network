8b18a9eda6805a5ef1d1d4aa625ec70afb94dc1e
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-06 20:46:35 -0700

    corrected authenticacion errors

--- app/static/js/register.js
+++ app/static/js/register.js
@@ -1,30 +1,28 @@
 document.getElementById('registerForm').addEventListener('submit', function (event) {
     const password = document.querySelector('input[name="password"]').value;
-    const passwordConfirm = document.querySelector('input[name="confirm_password"]').value;
+    const password_confirm = document.querySelector('input[name="confirm_password"]').value;
 
-    // Crear un elemento para mostrar errores si no existe
     let errorDiv = document.getElementById('error-message');
-    if (!errorDiv) {
+    if(!errorDiv) {
         errorDiv = document.createElement('div');
         errorDiv.id = 'error-message';
-        errorDiv.style.color = 'red';
+        error.style.color = 'red';
         document.getElementById('registerForm').prepend(errorDiv);
     }
 
-    // Limpiar mensajes de error previos
     errorDiv.textContent = '';
 
-    // Validar longitud de la contraseña
-    if (password.length < 6) {
+    if(password.length < 6){
         errorDiv.textContent = 'La contraseña debe tener al menos 6 caracteres.';
         event.preventDefault();
         return;
     }
 
-    // Validar que las contraseñas coincidan
-    if (password !== passwordConfirm) {
+    if(password !== password_confirm){
         errorDiv.textContent = 'Las contraseñas no coinciden.';
         event.preventDefault();
         return;
+
     }
+
 });
