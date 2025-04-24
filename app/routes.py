d12da384f6147708f44736afdc8a6eb996a9050d
RamsesSosa04 <sosaramses08@gmail.com>
2025-04-23 23:02:38 -0700

    modified folders

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
 from app.models import db, User, Post
-from werkzeug.security import generate_password_hash
 
 main = Blueprint('main', __name__)
 
